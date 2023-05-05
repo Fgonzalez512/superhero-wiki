3ca91c0a4871b829847a4a6711b231848a017a4b
Frank Gonzalez <frank.gonzalez628@gmail.com>
2023-05-05 14:38:06 -0500

    init

---
+++ src/pages/api/superhero-api.ts
@@ -0,0 +1,33 @@
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const CORS = 'https://cors-anywhere.herokuapp.com/';
+const SUPERHERO_API = `https://superheroapi.com/api/${API_KEY}`;
+
+export async function getRandomCharacters() {
+  try {
+    const randomIdArray = Array.from({ length: 4 }, () =>
+      Math.floor(Math.random() * 731)
+    );
+    const data = await Promise.all(
+      randomIdArray.map(async (id) => {
+        const response = await fetch(`${CORS}${SUPERHERO_API}/${id}`);
+        return response.json();
+      })
+    );
+    return { data };
+  } catch (error) {
+    console.error(error);
+    return { error: 'API ERROR' };
+  }
+}
+
+export async function getRandomCharacter() {
+  try {
+    const randomId = Math.floor(Math.random() * 731) + 1;
+    const response = await fetch(`${CORS}${SUPERHERO_API}/${randomId}`);
+    const data = await response.json();
+    return { data };
+  } catch (error) {
+    console.error(error);
+    return { error: 'API ERROR' };
+  }
+}
